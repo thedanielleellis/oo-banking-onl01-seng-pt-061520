725b56424a01c51a5b08b5faabd21fdcd08719e2
Danielle Ellis <thedanielleellis@gmail.com>
2020-07-21 06:09:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,10 @@
 class BankAccount
 attr_accessor:name 
-def initialize (name)
+def initialize (name, balance, status)
+  @name= name 
+  @balance =+ 1000
+  @status = open 
+  
+ def deposit
+ end
 end
